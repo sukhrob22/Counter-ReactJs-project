23e82c53da547831623bdbde46f5fde7be30fb18
sukhrob22 <ergashevsuxrob986@gmail.com>
2022-03-26 00:26:35 +0500

    Counter ReactJs project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react";
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import Counter from "./components/counter/Counter";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    };
+    const decreament = () => {
+        setCounter(counter - 1);
+    };
+    const refresh = ()=> {
+        setCounter(0)
+    }
+    
+    return ( <div className = "App" > 
+   
+    <Counter refresh={refresh} decreament={decreament} increment={increment} counter={counter}/>
+   
+    </div> );
+}
 export default App;
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,31 @@
+const Counter = (props) => {
+  const { increment, decreament, refresh, counter } = props;
+  let width = 600;
+  let logoUrl =
+    "https://tvfinternational.com/assets/Uploads/_resampled/FillWyIxOTAwIiwiMTAwMCJd/photo-1444703686981-a3abbc4d4fe4.jpg";
+
+  return (
+    <div className="myContainer">
+      <img width={width} src={logoUrl} />
+      <div>
+        <h1>gitCount the stars</h1>
+        <br></br>
+        <h1 id="value" style={{ fontSize: "100px", textAlign: "center" }}>
+          {counter}
+        </h1>
+        <br></br>
+        <button className="btn btn-danger m-3" onClick={decreament}>
+          -
+        </button>
+        <button className="btn btn-dark m-3 " onClick={refresh}>
+          restart
+        </button>
+        <button className="btn btn-success m-3" onClick={increment}>
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
